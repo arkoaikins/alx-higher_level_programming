1f8b4354d35b50c5a525f3c0738603b5b023252c
arkoaikins <arkoaikins@gmail.com>
2023-05-09 23:15:29 +0000

    [bug2 fixed]:print correct output when less than 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,5 +9,5 @@ if l_digit > 5:
 elif l_digit == 0:
     print(f"Last digit of {number:d} is {l_digit:d} and is 0")
 else:
-    print(f"Last digit of {number:d} is {l_digit:d} and is less"
+    print(f"Last digit of {number:d} is {l_digit:d} and is less "
           f"than 6 and not 0")
