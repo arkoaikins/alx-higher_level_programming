f9d1e5e59769000926cc7a0fa72ece1193e00bb2
arkoaikins <arkoaikins@gmail.com>
2023-05-10 19:35:12 +0000

    print alphebets in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 """
-program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase (z in lowercase and Y in uppercase) , not followed by a new line.
+program that prints the ASCII alphabet, in reverse order, alternating
+lowercase and uppercase (z in lowercase and Y in uppercase),
+not followed by a new line.
 """
 
 
