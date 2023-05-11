7c90acdfe599a3381c9580a655a4d7493be198a5
arkoaikins <arkoaikins@gmail.com>
2023-05-11 22:36:18 +0000

    [bug fixd]:print correct output

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -8,11 +8,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    adding = add(a, b)
-    minus = sub(a, b)
-    multiply = mul(a, b)
-    divide = div(a, b)
-    print("{} + {} = {}".format(a, b, adding))
-    print("{} - {} = {}".format(a, b, minus))
-    print("{} * {} = {}".format(a, b, multiply))
-    print("{} / {} = {}".format(a, b, divide))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
