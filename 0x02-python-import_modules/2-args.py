e0cad54b5d7e3d6b2c4dbe626a2bec38f0909a07
arkoaikins <arkoaikins@gmail.com>
2023-05-11 23:21:41 +0000

    create program that print argument list.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,17 +4,17 @@ program that displays the number of and the list of its arguments.
 """
 
 if __name__ == "__main__":
-    from sys import argv 
+    from sys import argv
     counter = len(argv)
-    #when no argument is passed
+    # when no argument is passed
     if counter == 1:
         print("{} argument.".format(counter - 1))
-    #when 1 argument is passed
+    # when 1 argument is passed
     elif counter == 2:
         print("{} argument:".format(counter - 1))
-    #when more than 1 argument is passed
+    # when more than 1 argument is passed
     else:
         print("{} arguments:".format(counter - 1))
-    #display number of argument and argument name
+    # display number of argument and argument name
     for i in range(1, counter):
-        print("{}: {}".format(i ,argv[i]))
+        print("{}: {}".format(i, argv[i]))
