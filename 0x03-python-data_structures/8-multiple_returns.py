ee82e5aa97036dea097c73c2d0e2b9c42479fa9d
arkoaikins <arkoaikins@gmail.com>
2023-05-15 22:13:14 +0000

    Create  a function that finds the biggest integer of a list

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a function that finds the biggest integer of a list.
+If the list is empty it return None
+"""
+
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_int = min(my_list)
+    for i in my_list:
+        if i > max_int:
+            max_int = i
+            return max_int
