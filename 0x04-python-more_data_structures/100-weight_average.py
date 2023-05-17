0340e98be8d896a0a5778fa9fa8b2bf8fef56903
arkoaikins <arkoaikins@gmail.com>
2023-05-17 22:30:16 +0000

    [pycodestlye check]:corrected block comment

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,10 +3,12 @@
  a function that returns the weighted average of
  all integers tuple (<score>, <weight>)
 """
+
+
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    #else return weitght average
+    # else return weitght average
     return sum([mul(x[0], x[1]) for x in my_list]) / sum(x[1] for x in my_list)
 
 
