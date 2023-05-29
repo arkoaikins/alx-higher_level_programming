02438b2b95ea1cdfe829cdcb8b3dda23db5e732b
arkoaikins <arkoaikins@gmail.com>
2023-05-29 15:45:25 +0000

    does exactly the same as  Python bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ magic_calculation - does exactly the same as a Python bytecode
+"""
+
+
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return (result)
