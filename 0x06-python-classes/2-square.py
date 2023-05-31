014d6fea6f79b6d8a645d5566a87578a46d8e375
arkoaikins <arkoaikins@gmail.com>
2023-05-31 14:08:36 +0000

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,10 +4,10 @@ class Square that defines a square by: (based on 1-square.py)
 """
 
 
-class Square():
+class Square:
     def __init__(self, size=0):
         """instantiation with optional"""
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         """raises type error exception with a message"""
         if size < 0:
