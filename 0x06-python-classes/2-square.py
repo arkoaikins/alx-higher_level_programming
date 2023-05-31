7e30afbe991c8b94263304ba6be365cb0aba54be
arkoaikins <arkoaikins@gmail.com>
2023-05-31 13:57:29 +0000

    a class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+class Square that defines a square by: (based on 1-square.py)
+"""
+
+
+class Square():
+    def __init__(self, size=0):
+        """instantiation with optional"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        """raises type error exception with a message"""
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        """raises valueerror exception with a message"""
+        self.__size = size
