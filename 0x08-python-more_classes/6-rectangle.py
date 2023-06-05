8f5b24c56c4185ab408a8db54c36e197758a6867
arkoaikins <arkoaikins@gmail.com>
2023-06-05 21:56:07 +0000

    set number of instances to Zero

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     """
     class rectange with height and width attributes
     """
-    number_of_instances = 2
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
