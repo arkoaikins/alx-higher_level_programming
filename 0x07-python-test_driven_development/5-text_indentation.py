561ec27202244e24be0bb6d24439a6766cb399ed
arkoaikins <arkoaikins@gmail.com>
2023-06-09 23:14:43 +0000

    a function that prints a text with 2 new lines after each of these characters: ., ? and :

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-a function that prints a text with 2 new lines after each of these characters: ., ? and :
+a function that prints a text with 2 new lines after each of these
+characters: ., ? and :
 """
+
 
 def text_indentation(text):
     """
