ddee130e99f0dd4e1bc3548649c9dd2019a43f13
arkoaikins <arkoaikins@gmail.com>
2023-06-19 08:28:10 -0700

    Update the class Rectangle by adding setter methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,3 +29,83 @@ class Rectangle(Base):
 
         task 2 ends here
         """
+
+    @property
+    def width(self):
+        """
+        task 2:
+        assigning the width argument to the right attribute
+        """
+        return self.__width
+
+        """
+        Task 3
+        Updating the Class Reactangle by adding all setter instantiation
+        """
+    @width.setter
+    def width(self, value):
+        """
+        adding the width setter with exceptions
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        task 2:
+        assigning the height argument to the right attribute
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        adding the height setter with exceptions
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        assigning the x argument to the right attribute
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        adding the height setter with exceptions
+        """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        task 2:
+        assigning the y argument to the right attribute
+
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        adding the height setter with exceptions
+        """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
