7ebabd541850cf187187556e7bc8538465b6b743
arkoaikins <arkoaikins@gmail.com>
2023-09-21 16:26:35 -0700

    Write a script that lists all State objects, and corresponding City objects, contained in the database

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all State objects, and corresponding
+City objects, contained in the database
+"""
+
+if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import relationship
+
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
