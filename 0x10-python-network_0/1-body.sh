bcbc8da33329e873f0c2c0253f5de73877edcdc4
arkoaikins <arkoaikins@gmail.com>
2023-09-29 01:54:34 +0000

    write bash script that display the body of response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
 # Write a Bash script that takes in a URL, sends a GET request to the URL,
 # and displays the body of the response
 # Display only body of a 200 status code response
-curl -Ls "$1"
+curl -sL "$1"
