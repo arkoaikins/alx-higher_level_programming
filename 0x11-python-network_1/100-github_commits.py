291643c5346b6818902e62f2d1b06993078b27ff
arkoaikins <arkoaikins@gmail.com>
2023-09-30 00:39:21 +0000

    solve challenge

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Python script that takes 2 arguments to solve this challenge
+challenge is below
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
+
+
+"""
+Please list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails” You must use the GitHub API, here is the
+documentation https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
