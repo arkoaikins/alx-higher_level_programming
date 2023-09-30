5001c0087efcf04c7731e2a17234ca599182eed8
arkoaikins <arkoaikins@gmail.com>
2023-09-30 00:44:49 +0000

    [solve challenge]-push the executable one

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -19,11 +19,3 @@ if __name__ == "__main__":
                 commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
-
-
-"""
-Please list 10 commits (from the most recent to oldest) of the repository
-“rails” by the user “rails” You must use the GitHub API, here is the
-documentation https://developer.github.com/v3/repos/commits/
-Print all commits by: `<sha>: <author name>` (one by line)
-"""
