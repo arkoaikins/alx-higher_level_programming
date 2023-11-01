0533b7a48833b17fcc544d4ad460e39125f444cb
arkoaikins <arkoaikins@gmail.com>
2023-11-01 19:46:31 +0000

    Write a JavaScript script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello.

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+/*
+ * Write a JavaScript script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr
+ * and displays the value of hello from that fetch in the HTML tag DIV#hello.
+ */
+$.getJSON("https://fourtonfish.com/hellosalut/?lang=fr", function(resp){
+    $("#hello").text(resp.hello);
