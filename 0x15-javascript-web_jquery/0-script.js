2b7dfea46e0930b087627aef03851709830aff47
arkoaikins <arkoaikins@gmail.com>
2023-11-01 18:21:22 +0000

    wrtie script that updates the text color of the <header> element to red (#FF0000)

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+/*
+ * JavaScript script that updates the text color of the
+ * <header> element to red (#FF0000):
+ */
+const headerEle = document.querySelector('header');
+headerEle.style.color = '#FF0000';
